9dd3d11ab9beb41f3de5b1fbfc42d59aea88e6d4
Artem <griukov@protonmail.com>
2021-06-09 22:52:36 +0300

    tests for GET /user

--- user/src/routes/user/get.rs
+++ user/src/routes/user/get.rs
@@ -15,3 +15,42 @@ where
 
     HttpResponse::NotImplemented().finish()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_rt;
+    use crate::dao::mocks::*;
+    use crate::fixtures::*;
+    use actix_web::http::StatusCode;
+
+    #[actix_rt::test]
+    async fn get_user_test() {
+        let mut user_dao = MockUserDAO::new();
+        let refresh_token_dao = MockRefreshTokenDAO::new();
+
+        user_dao.expect_get_user().returning(move |_| {
+            Some(user_fixture())
+        });
+
+        let context = build_context(user_dao, refresh_token_dao);
+        let result = route(web::Path::from("/user/user-id".to_string()), context).await;
+
+        assert_eq!(result.status(), StatusCode::OK);
+    }
+
+    #[actix_rt::test]
+    async fn get_nonexisting_user_test() {
+        let mut user_dao = MockUserDAO::new();
+        let refresh_token_dao = MockRefreshTokenDAO::new();
+
+        user_dao.expect_get_user().returning(move |_| {
+            None
+        });
+
+        let context = build_context(user_dao, refresh_token_dao);
+        let result = route(web::Path::from("/user/user-id".to_string()), context).await;
+
+        assert_eq!(result.status(), StatusCode::NOT_FOUND);
+    }
+}
