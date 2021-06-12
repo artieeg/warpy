b8f386e4718c87561b2ac1ebf55b451c03ff3d28
Artem <griukov@protonmail.com>
2021-06-12 15:16:15 +0300

    streams: POST /streams route implementation

--- streams/src/models/stream.rs
+++ streams/src/models/stream.rs
@@ -1,4 +1,6 @@
 use serde::{Serialize, Deserialize};
+use crate::payloads::CreateStreamPayload;
+use nanoid::nanoid;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Stream {
@@ -7,4 +9,16 @@ pub struct Stream {
     pub hub: String,
     pub live: bool,
     pub title: String
+}
+
+impl Stream {
+    pub fn from_payload(payload: CreateStreamPayload, owner: &str) -> Self {
+        Self {
+            id: nanoid!(),
+            owner: owner.to_string(),
+            hub: payload.hub,
+            title: payload.title,
+            live: true,
+        }
+    }
 }
--- streams/src/routes/streams/create.rs
+++ streams/src/routes/streams/create.rs
@@ -1,6 +1,8 @@
 use crate::context::WarpyContext;
 use crate::dao::*;
+use crate::models::Stream;
 use crate::payloads::CreateStreamPayload;
+use serde_json::json;
 use actix_web::{web, HttpRequest, HttpResponse};
 use std::sync::Mutex;
 
@@ -13,7 +15,25 @@ where
     S: StreamDAOExt,
     H: HubDAOExt,
 {
-    HttpResponse::NotImplemented().finish()
+    let user = req.headers().get("user-id").unwrap().to_str().unwrap();
+    let data = data.lock().unwrap();
+
+    //Check if hub id is valid
+    let hub_id = payload.hub.as_str();
+    if !data.hub_dao.exists(hub_id).await {
+        return HttpResponse::BadRequest().finish();
+    }
+
+    let already_streaming = data.stream_dao.is_user_live(user).await;
+    if already_streaming {
+        return HttpResponse::Conflict().finish();
+    }
+
+    let stream = Stream::from_payload(payload.into_inner(), user);
+    match data.stream_dao.create(stream).await {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(e) => e.into()
+    }
 }
 
 #[cfg(test)]
@@ -36,7 +56,7 @@ mod tests {
         let mut hub_dao = MockHubDAO::new();
 
         hub_dao.expect_exists().returning(|_| true);
-        stream_dao.expect_is_user_live().returning(|_| true);
+        stream_dao.expect_is_user_live().returning(|_| false);
         stream_dao.expect_create().returning(|_| Ok(()));
 
         let context = build_context(stream_dao, hub_dao);
