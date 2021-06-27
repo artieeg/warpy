7a279bc7f5abe47f803e08f024e3907339e494f0
Artem <griukov@protonmail.com>
2021-06-27 08:37:24 +0300

    warpy: publish local stream

--- warpy/lib/viewmodels/new_stream_viewmodel.dart
+++ warpy/lib/viewmodels/new_stream_viewmodel.dart
@@ -12,10 +12,9 @@ class NewStreamViewModel extends ChangeNotifier {
   final localRenderer = RTCVideoRenderer();
   late ion.LocalStream localStream;
   bool localViewInitialized = false;
-  final ion.IonBaseConnector connector = ion.IonBaseConnector(Constants.ION);
   final user = locator<UserService>().user;
-  late ion.IonAppBiz biz;
-  late ion.IonSDKSFU sfu;
+  late ion.Signal signal;
+  late ion.Client client;
   String? streamId;
 
   Future<void> stopStream() async {
@@ -25,36 +24,18 @@ class NewStreamViewModel extends ChangeNotifier {
   }
 
   Future<void> initPion() async {
-    var userId = user.id;
+    signal = ion.GRPCWebSignal(Constants.ION);
+    client = await ion.Client.create(
+          sid: streamId!, uid: user.id, signal: signal);
 
-    biz = ion.IonAppBiz(connector);
-    sfu = ion.IonSDKSFU(connector);
+    localStream = await ion.LocalStream.getUserMedia(
+        constraints: ion.Constraints.defaults..simulcast = true..codec = "vp8");
 
-    biz.onJoin = (bool joined, String reason) async {
-      if (!joined) {
-        //TODO: handle
-      }
+    localViewInitialized = true;
+    client.publish(localStream);
 
-      await sfu.connect();
-      await sfu.join(streamId!, user.id);
-
-      localStream = await ion.LocalStream.getUserMedia(
-          constraints: ion.Constraints.defaults..simulcast = true..codec = "vp8");
-
-      localViewInitialized = true;
-
-      Future.delayed(Duration(milliseconds: 400), () {
-        sfu.publish(localStream);
-      });
-
-      localRenderer.srcObject = localStream.stream;
-      notifyListeners();
-    };
-
-    await biz.connect();
-    biz.join(info: user.toMap(), sid: streamId!, uid: userId);
-
-    print("connecting to ion...");
+    localRenderer.srcObject = localStream.stream;
+    notifyListeners();
   }
 
   void initLocalRenderer() async {
