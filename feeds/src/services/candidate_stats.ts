852a298ba25a8797d8bbe2794020fb05a5f288d2
Artem <griukov@protonmail.com>
2021-07-04 15:15:36 +0300

    feeds: get sorted streams by score

--- feeds/src/services/candidate_stats.ts
+++ feeds/src/services/candidate_stats.ts
@@ -1,4 +1,3 @@
-import { IStats } from "@app/models";
 import redis from "redis";
 
 const URL = process.env.STATS_CACHE || "redis://127.0.0.1:6379/2";
@@ -11,9 +10,13 @@ const client = redis.createClient({
 
 export const connect = () => {};
 
-const addScore = async (streamId: string) => {
+const addScore = async (streamId: string, hub?: string) => {
   return new Promise<void>((resolve) => {
-    client.zadd("scores", "NX", 0, streamId);
+    client.zadd("main-feed", "NX", 0, streamId);
+
+    if (hub) {
+      client.zadd(hub, "NX", 0, streamId);
+    }
 
     resolve();
   });
@@ -34,13 +37,23 @@ const initCandidateStats = async (streamId: string) => {
 /**
  * Initialize stats for candidate
  */
-export const createStats = async (streamId: string) => {
-  return Promise.all([addScore(streamId), initCandidateStats(streamId)]);
+export const createStats = async (streamId: string, hub?: string) => {
+  return Promise.all([addScore(streamId, hub), initCandidateStats(streamId)]);
 };
 
 /*
  * Returns sorted by score stream ids from Redis sorted set
  */
 export const getSortedStreamIds = async (hub?: string): Promise<string[]> => {
-  return [];
+  return new Promise((resolve, reject) => {
+    const key = hub || "scores";
+
+    client.zrangebyscore(key, 0, 1, (err, result) => {
+      if (err) {
+        return reject(err);
+      }
+
+      resolve(result);
+    });
+  });
 };
