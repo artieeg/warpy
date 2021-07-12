ab8abf2967d66613b4617df92c3a4d8417b540b1
Artem <griukov@protonmail.com>
2021-07-12 18:48:44 +0300

    video: new service

---
+++ video/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@app/(.*)$': '<rootDir>/src/$1',
+  },
+  setupFilesAfterEnv: [
+    "./jest.setup.ts"
+  ]
+};
