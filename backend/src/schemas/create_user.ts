601b5af9415c19403728a38cc1710ed97858e3ef
Artem <griukov@protonmail.com>
2021-08-15 08:28:55 +0300

    backend: remove http schemas

--- backend/src/schemas/create_user.ts
+++
@@ -1,8 +0,0 @@
-import * as yup from "yup";
-
-export const createDevUserSchema = yup.object().shape({
-  username: yup.string().required(),
-  last_name: yup.string().required(),
-  first_name: yup.string().required(),
-  email: yup.string().required(),
-});
