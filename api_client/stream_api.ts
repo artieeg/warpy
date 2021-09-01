345f29baac06cf1f2238e14866a07ee676b3c8fb
Artem <griukov@protonmail.com>
2021-09-01 12:05:35 +0300

    api_client: added claps update listener

--- api_client/stream_api.ts
+++ api_client/stream_api.ts
@@ -1,11 +1,12 @@
-import {APIModule, EventHandler} from './types';
+import { APIModule, EventHandler } from "./types";
 import {
   IActiveSpeakerEvent,
+  IClapsUpdate,
   IJoinStreamResponse,
   INewStreamResponse,
   IRequestViewersResponse,
   ISpeakingAllowedEvent,
-} from '@warpy/lib';
+} from "@warpy/lib";
 
 export interface IStreamAPI {
   create: (title: string, hub: string) => Promise<INewStreamResponse>;
@@ -13,10 +14,11 @@ export interface IStreamAPI {
   stop: (stream: string) => any;
   getViewers: (
     stream: string,
-    page: number,
+    page: number
   ) => Promise<IRequestViewersResponse>;
   raiseHand: () => any;
   allowSpeaker: (speaker: string) => any;
+  onClapsUpdate: EventHandler<IClapsUpdate>;
   onNewViewer: EventHandler;
   onNewRaisedHand: EventHandler;
   onUserLeft: EventHandler;
@@ -25,22 +27,23 @@ export interface IStreamAPI {
   onActiveSpeaker: EventHandler<IActiveSpeakerEvent>;
 }
 
-export const StreamAPI: APIModule<IStreamAPI> = socket => ({
+export const StreamAPI: APIModule<IStreamAPI> = (socket) => ({
   create: (title, hub) =>
-    socket.request('stream-new', {
+    socket.request("stream-new", {
       title,
       hub,
     }),
-  stop: stream => socket.publish('stream-stop', {stream}),
-  join: stream => socket.request('join-stream', {stream}),
+  stop: (stream) => socket.publish("stream-stop", { stream }),
+  join: (stream) => socket.request("join-stream", { stream }),
   getViewers: (stream, page) =>
-    socket.request('request-viewers', {stream, page}),
-  raiseHand: () => socket.publish('raise-hand', {}),
-  allowSpeaker: speaker => socket.publish('speaker-allow', {speaker}),
-  onNewViewer: handler => socket.on('new-viewer', handler),
-  onNewRaisedHand: handler => socket.on('raise-hand', handler),
-  onUserLeft: handler => socket.on('user-left', handler),
-  onNewSpeaker: handler => socket.on('new-speaker', handler),
-  onActiveSpeaker: handler => socket.on('active-speaker', handler),
-  onSpeakingAllowed: handler => socket.on('speaking-allowed', handler),
+    socket.request("request-viewers", { stream, page }),
+  raiseHand: () => socket.publish("raise-hand", {}),
+  allowSpeaker: (speaker) => socket.publish("speaker-allow", { speaker }),
+  onNewViewer: (handler) => socket.on("new-viewer", handler),
+  onNewRaisedHand: (handler) => socket.on("raise-hand", handler),
+  onUserLeft: (handler) => socket.on("user-left", handler),
+  onNewSpeaker: (handler) => socket.on("new-speaker", handler),
+  onActiveSpeaker: (handler) => socket.on("active-speaker", handler),
+  onSpeakingAllowed: (handler) => socket.on("speaking-allowed", handler),
+  onClapsUpdate: (handler) => socket.on("claps-update", handler),
 });
