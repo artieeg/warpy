ba3023ca93e1ea77308804ef726b1a0481bde3c4
Artem <griukov@protonmail.com>
2021-09-20 23:40:13 +0300

    backend: check if user has been banned from the room when joining

--- backend/src/services/stream/removeUserFromStreams.ts
+++ backend/src/services/stream/removeUserFromStreams.ts
@@ -1,17 +1,19 @@
-import { ParticipantDAL } from "@backend/dal";
+import { ParticipantDAL, StreamDAL } from "@backend/dal";
 import { BroadcastService, FeedCacheService } from "..";
 
 export const removeUserFromStreams = async (user: string): Promise<void> => {
   const stream = await ParticipantDAL.getCurrentStreamFor(user);
 
   try {
-    if (stream) {
-      BroadcastService.broadcastParticipantLeft(user, stream);
-    }
-
     await ParticipantDAL.updateOne(user, {
       hasLeftStream: true,
     });
+
+    if (stream) {
+      BroadcastService.broadcastParticipantLeft(user, stream);
+      await StreamDAL.delete(stream);
+    }
+
     await FeedCacheService.removeServedStreams(user);
   } catch (e) {
     console.error(e);
