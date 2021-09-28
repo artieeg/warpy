848a82c2df302aa182268ae62a5d67faecbe61c4
Artem <griukov@protonmail.com>
2021-09-28 21:17:41 +0300

    join a stream from notification

--- warpy/src/components/Notification.tsx
+++ warpy/src/components/Notification.tsx
@@ -1,35 +1,47 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, TouchableWithoutFeedback} from 'react-native';
 import {IInvite, INotification} from '@warpy/lib';
 import {Avatar} from './Avatar';
 import {Text} from './Text';
+import {useNavigation} from '@react-navigation/native';
 
 interface INotificationProps {
   notification: INotification;
 }
 
-const InviteNotification = ({invite}: {invite: IInvite}) => (
-  <View style={styles.wrapper}>
-    <Avatar user={invite.inviter} />
-    <View style={styles.info}>
-      <Text size="xsmall" weight="bold">
-        {invite.inviter.username}
-      </Text>
-      <Text size="xsmall" color="white" weight="bold">
-        invited you to join the {invite.stream.title} live stream
-      </Text>
-    </View>
-  </View>
-);
+const InviteNotification = ({invite}: {invite: IInvite}) => {
+  const navigation = useNavigation();
+
+  return (
+    <TouchableWithoutFeedback
+      onPress={() => {
+        navigation.navigate('Stream', {stream: invite.stream});
+      }}>
+      <View style={styles.wrapper}>
+        <Avatar user={invite.inviter} />
+        <View style={styles.info}>
+          <Text size="xsmall" weight="bold">
+            {invite.inviter.username}
+          </Text>
+          <Text size="xsmall" color="white" weight="bold">
+            invited you to join the {invite.stream.title} live stream
+          </Text>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
 
 export const Notification = ({notification}: INotificationProps) => {
   const {invite} = notification;
 
-  if (invite) {
-    return <InviteNotification invite={invite} />;
-  }
+  const renderNotificationContent = () => {
+    if (invite) {
+      return <InviteNotification invite={invite} />;
+    }
+  };
 
-  return <View></View>;
+  return <View>{renderNotificationContent()}</View>;
 };
 
 const styles = StyleSheet.create({
