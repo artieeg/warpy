c5b8f85d7fc3e03e94470ae05a4c6bcad6793a74
Artem <griukov@protonmail.com>
2021-10-30 09:41:53 +0300

    remove bots_api

--- bots_api/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { BotsModule } from './bots/bots.module';
-
-@Module({
-  imports: [BotsModule],
-  controllers: [],
-  providers: [],
-})
-export class AppModule {}
--- bots_api/src/bots/bots.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class BotService {
-  createNewBot() {
-    return 'sign up route inc';
-  }
-}
