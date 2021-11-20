82ab32b1ad01c01176e3ce5494fa9ed1e07c2b47
Artem <griukov@protonmail.com>
2021-11-20 14:50:43 +0200

    awards table

---
+++ backend/prisma/migrations/20211120124632_award_item_table/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "AwardItem" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "media" TEXT NOT NULL,
+    "price" INTEGER NOT NULL,
+
+    PRIMARY KEY ("id")
+);
---
+++ backend/prisma/migrations/20211120124648_init_awards/migration.sql
@@ -0,0 +1,3 @@
+-- This is an empty migration.
+
+INSERT into public."AwardItem" (id, title, media, price) values ('0', 'pogslime', 'https://warpy.s3.eu-central-1.amazonaws.com/award0.png', 100);
