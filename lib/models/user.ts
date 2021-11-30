8ae1d88fcd5c67101279b2c07faefc0fe46d505a
Artem <griukov@protonmail.com>
2021-11-30 10:11:27 +0200

    clean up

--- lib/models/user.ts
+++ lib/models/user.ts
@@ -12,31 +12,3 @@ export interface IUser extends IBaseUser {
   email: string | null;
   sub: string | null;
 }
-
-/*
-export class BaseUser implements IBaseUser {
-  id: string;
-  last_name: string;
-  first_name: string;
-  username: string;
-  avatar: string;
-
-  constructor(data: IBaseUser) {
-    this.id = data.id;
-    this.last_name = data.last_name;
-    this.first_name = data.first_name;
-    this.username = data.username;
-    this.avatar = data.avatar;
-  }
-
-  static fromJSON(data: any) {
-    return new BaseUser({
-      id: data.id,
-      last_name: data.last_name,
-      first_name: data.first_name,
-      username: data.username,
-      avatar: data.avatar,
-    });
-  }
-}
-*/
