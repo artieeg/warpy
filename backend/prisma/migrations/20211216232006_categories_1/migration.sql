12d6f3efd613bf63e4bc18b9a48fc9a53ed3c919
Artem <griukov@protonmail.com>
2021-12-17 01:30:58 +0200

    fill up the database with initial values

---
+++ backend/prisma/migrations/20211216232006_categories_1/migration.sql
@@ -0,0 +1,11 @@
+-- This is an empty migration.
+
+INSERT into public."StreamCategory" (id, value, priority) values ('0', 'daily', 0);
+INSERT into public."StreamCategory" (id, value, priority) values ('1', 'comedy', 1);
+INSERT into public."StreamCategory" (id, value, priority) values ('2', 'debates', 2);
+INSERT into public."StreamCategory" (id, value, priority) values ('3', 'talent', 3);
+INSERT into public."StreamCategory" (id, value, priority) values ('4', 'pets', 4);
+INSERT into public."StreamCategory" (id, value, priority) values ('5', 'travel', 5);
+INSERT into public."StreamCategory" (id, value, priority) values ('6', 'games', 6);
+INSERT into public."StreamCategory" (id, value, priority) values ('7', 'news', 7);
+INSERT into public."StreamCategory" (id, value, priority) values ('8', 'other', 8);
