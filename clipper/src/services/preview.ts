dc067622ac58e8760699e0dcf314b0f160f79a9c
Artem <griukov@protonmail.com>
2022-01-08 21:40:24 +0200

    clipper: work on previews

--- clipper/src/services/preview.ts
+++ clipper/src/services/preview.ts
@@ -1,7 +1,6 @@
 import { EventEmitter } from "events";
-import { IRecordRequest } from "@warpy/lib";
-import { createLoopedVideo } from "./looper";
-import { clips, createMediaRecorder } from "./recorder";
+import { spawn } from "child_process";
+import { clips } from "./recorder";
 import { removeFile } from "./cleaner";
 
 type PreviewProducer = {
@@ -14,16 +13,16 @@ type PreviewProducer = {
 export const startProducingPreviews = (stream: string): PreviewProducer => {
   const observer = new EventEmitter();
 
-  setInterval(() => {
-    //TODO: ARRANGE VIDS
+  setInterval(async () => {
+    const sources = Object.values(clips[stream]).map(
+      ({ directory, filename }) => directory + filename
+    );
 
-    const streams = Object.values(clips[stream]);
+    const preview = await produceStreamPreview(stream, sources);
 
-    streams.forEach(async ({ directory, filename }) => {
-      const loopedVideo = await createLoopedVideo(directory, filename);
-      console.log("looped vidoe", loopedVideo);
-    });
+    console.log({ preview });
   }, 10000);
+
   /*
   clipRecordManager.onClipReady((clipInfo) => {
     const loopManager = createLoopedVideo(
@@ -48,4 +47,35 @@ export const startProducingPreviews = (stream: string): PreviewProducer => {
       removeFile(preview.directory + preview.filename);
     },
   };
+};
+
+const filters = [
+  "[0]reverse[r];[0][r]concat=n=2:v=1:a=0,setpts=N/55/TB", //one stream -- just loop and speed up
+  "[0:v][1:v]vstack=inputs=2[v]", //two streams -- stack
+  "[0:v][1:v][2:v]vstack=inputs=3[v]", //three streams -- stack
+  "", //TODO
+];
+
+const produceStreamPreview = async (id: string, sources: string[]) => {
+  let args: string[] = [];
+
+  sources.forEach((source) => {
+    args.push("-i");
+    args.push(source);
+  });
+
+  const output = `./recordings/preview_${id}.webm`;
+
+  const filter = filters[sources.length - 1];
+  args.push("-filter_complex");
+  args.push(filter);
+  args.push(output);
+
+  const process = spawn("ffmpeg", args);
+
+  return new Promise<string>((resolve) => {
+    process.on("close", () => {
+      resolve(output);
+    });
+  });
 };
