e9bc095430db570d48a296ec4d9281ce585cd516
Artem <griukov@protonmail.com>
2022-01-10 23:47:11 +0200

    backend: gifs should come from tenor.com

--- backend/src/award/award.service.ts
+++ backend/src/award/award.service.ts
@@ -1,5 +1,5 @@
 import { CoinBalanceEntity } from '@backend_2/coin-balance/coin-balance.entity';
-import { NotEnoughCoins } from '@backend_2/errors';
+import { InvalidVisual, NotEnoughCoins } from '@backend_2/errors';
 import { Injectable } from '@nestjs/common';
 import { EventEmitter2 } from '@nestjs/event-emitter';
 import { AwardEntity } from './award.entity';
@@ -16,13 +16,21 @@ export class AwardService {
     return this.awardEntity.getByRecipent(user);
   }
 
+  private async validateVisual(visual: string) {
+    const url = new URL(visual);
+
+    if (!url.hostname.includes('tenor.com')) {
+      throw new InvalidVisual();
+    }
+  }
+
   async sendAward(
     sender: string,
     recipent: string,
     visual: string,
     message: string,
   ) {
-    console.log('sending', { sender, recipent, visual, message });
+    await this.validateVisual(visual);
 
     if (!(await this.coinBalanceEntity.check(sender, 300))) {
       throw new NotEnoughCoins();
