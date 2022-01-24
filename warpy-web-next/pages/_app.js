e66b9f74fea5da45fb7e5793380986117c1d6f98
Artem <griukov@protonmail.com>
2022-01-24 17:02:59 +0200

    disable user scaling

--- warpy-web-next/pages/_app.js
+++ warpy-web-next/pages/_app.js
@@ -17,12 +17,8 @@ function AppWrapper(props) {
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content={description} />
 
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+        <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no" />
         <meta name="theme-color" content="#000000" />
-        <meta
-          name="viewport"
-          content="initial-scale=1, viewport-fit=cover, width=device-width"
-        ></meta>
         <meta name="apple-mobile-web-app-capable" content="yes"></meta>
         <meta
           name="apple-mobile-web-app-status-bar-style"
