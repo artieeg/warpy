489245f9df753a93b99ea66a1f1c85ee86fa7673
Artem <griukov@protonmail.com>
2022-01-25 23:12:57 +0200

    twilio env variables + warn if missing some env vars

--- backend/src/config/configuration.ts
+++ backend/src/config/configuration.ts
@@ -1,3 +1,5 @@
+export const REQUIRED_ENV_KEYS = ['TENOR_KEY', 'TWILIO_SID', 'TWILIO_AUTH'];
+
 export const configuration = () => ({
   accessJwtSecret: process.env.JWT_SECRET || 'test-secret',
   tenorAPIKey: process.env.TENOR_KEY,
@@ -6,4 +8,6 @@ export const configuration = () => ({
     process.env.USER_ONLINE_STATUS ?? 'redis://127.0.0.1/7',
   mediaServerIdsCache: process.env.MEDIA_SERVER_IDS ?? 'redis://127.0.0.1/6',
   nats: process.env.NATS_ADDR ?? '127.0.0.1',
+  twilioSID: process.env.TWILIO_SID,
+  twilioAuth: process.env.TWILIO_AUTH,
 });
