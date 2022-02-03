1865b72275e1f4c2d7e2268405eb0a3238023431
Artem <griukov@protonmail.com>
2022-02-04 00:26:00 +0200

    RoundButton animations

--- warpy/src/components/RoundButton.tsx
+++ warpy/src/components/RoundButton.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
 import {StyleSheet, TouchableOpacity} from 'react-native';
+import Animated, {
+  call,
+  Easing,
+  useAnimatedStyle,
+  useSharedValue,
+  withSequence,
+  withTiming,
+} from 'react-native-reanimated';
 
 export interface IRoundButtonProps {
   children?: React.ReactChild;
@@ -10,10 +18,42 @@ export interface IRoundButtonProps {
 export const RoundButton = (props: IRoundButtonProps) => {
   const {children, onPress, style} = props;
 
+  const scale = useSharedValue(1);
+
+  const onPressWrapper = React.useCallback(() => {
+    scale.value = withSequence(
+      withTiming(0.9, {duration: 100, easing: Easing.ease}),
+      withTiming(1, {duration: 100, easing: Easing.ease}),
+    );
+
+    setTimeout(() => {
+      onPress();
+    }, 200);
+  }, [onPress]);
+
+  const onPressIn = React.useCallback(() => {
+    scale.value = withTiming(0.9, {duration: 200, easing: Easing.exp});
+  }, []);
+
+  const onPressOut = React.useCallback(() => {
+    scale.value = withTiming(1, {duration: 200, easing: Easing.exp});
+  }, []);
+
+  const animatedWrapperStyle = useAnimatedStyle(() => ({
+    transform: [{scale: scale.value}],
+  }));
+
   return (
-    <TouchableOpacity onPress={onPress} style={[styles.wrapper, style]}>
-      {children}
-    </TouchableOpacity>
+    <Animated.View style={animatedWrapperStyle}>
+      <TouchableOpacity
+        onPressOut={onPressOut}
+        onPressIn={onPressIn}
+        activeOpacity={1.0}
+        onPress={onPressWrapper}
+        style={[styles.wrapper, style]}>
+        {children}
+      </TouchableOpacity>
+    </Animated.View>
   );
 };
 
