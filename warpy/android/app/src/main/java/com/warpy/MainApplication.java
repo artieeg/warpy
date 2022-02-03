b3a3bcfe5c3d7435c0e72309b6163351a00a1e5c
Artem <griukov@protonmail.com>
2022-02-04 00:11:34 +0200

    added react-native-reanimated

--- warpy/android/app/src/main/java/com/warpy/MainApplication.java
+++ warpy/android/app/src/main/java/com/warpy/MainApplication.java
@@ -12,6 +12,9 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 import com.facebook.react.bridge.JSIModulePackage;
+import com.swmansion.reanimated.ReanimatedJSIModulePackage;
+
+import com.facebook.react.bridge.JSIModulePackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -34,6 +37,11 @@ public class MainApplication extends Application implements ReactApplication {
         @Override
         protected String getJSMainModuleName() {
           return "index";
+        }
+
+        @Override
+        protected JSIModulePackage getJSIModulePackage() {
+          return new ReanimatedJSIModulePackage();
         }
       };
 
