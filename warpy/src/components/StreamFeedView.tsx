a03b3c885cbf95152307cda8beac293df3197cad
Artem <griukov@protonmail.com>
2022-02-05 00:49:36 +0200

    work on feed ux

--- warpy/src/components/StreamFeedView.tsx
+++ warpy/src/components/StreamFeedView.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
-import {FlatList} from 'react-native';
+import {FlatList, useWindowDimensions, View} from 'react-native';
 import {ICandidate} from '@warpy/lib';
 import {usePreviewDimensions} from '@app/hooks';
 import {StreamPreview} from './StreamPreview';
+import Animated, {
+  Easing,
+  useAnimatedStyle,
+  useDerivedValue,
+  withTiming,
+} from 'react-native-reanimated';
 
 export const StreamFeedView: React.FC<{feed: ICandidate[]}> = ({feed}) => {
   const {previewHeight, previewWidth} = usePreviewDimensions();
@@ -32,5 +38,31 @@ export const StreamFeedView: React.FC<{feed: ICandidate[]}> = ({feed}) => {
     [],
   );
 
-  return <FlatList data={feed} numColumns={2} renderItem={renderItem} />;
+  const {height} = useWindowDimensions();
+
+  const count = feed?.length ?? 0;
+
+  const coverTranslateY = useDerivedValue(() => {
+    return withTiming(count > 0 ? height : 0, {
+      duration: 400,
+      easing: Easing.ease,
+    });
+  }, [height, count]);
+
+  const coverStyle = useAnimatedStyle(() => ({
+    transform: [{translateY: coverTranslateY.value}],
+    backgroundColor: '#ff3030',
+    position: 'absolute',
+    bottom: 0,
+    top: 0,
+    left: 0,
+    right: 0,
+  }));
+
+  return (
+    <View style={{flex: 1}}>
+      <FlatList data={feed} numColumns={2} renderItem={renderItem} />
+      <Animated.View style={coverStyle} />
+    </View>
+  );
 };
