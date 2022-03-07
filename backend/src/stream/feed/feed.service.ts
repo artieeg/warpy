ba50df6093bdd05bf6b48c43e0874b40d5270f84
Artem <griukov@protonmail.com>
2022-03-07 15:03:52 +0200

    use cached block info in feed candidates

--- backend/src/stream/feed/feed.service.ts
+++ backend/src/stream/feed/feed.service.ts
@@ -1,5 +1,5 @@
 import { StreamEntity } from '@backend_2/stream/common/stream.entity';
-import { BlockEntity } from '@backend_2/user/block/block.entity';
+import { BlockService } from '@backend_2/user/block/block.service';
 import { ParticipantEntity } from '@backend_2/user/participant/common/participant.entity';
 import { forwardRef, Inject, Injectable } from '@nestjs/common';
 import { ICandidate, IStream } from '@warpy/lib';
@@ -7,8 +7,8 @@ import { ICandidate, IStream } from '@warpy/lib';
 @Injectable()
 export class FeedService {
   constructor(
-    @Inject(forwardRef(() => BlockEntity))
-    private blockEntity: BlockEntity,
+    @Inject(forwardRef(() => BlockService))
+    private blockService: BlockService,
 
     private streamEntity: StreamEntity,
     private participantEntity: ParticipantEntity,
@@ -34,8 +34,8 @@ export class FeedService {
   }
 
   async getFeed(user: string, category?: string) {
-    const blockedUserIds = await this.blockEntity.getBlockedUserIds(user);
-    const blockedByUserIds = await this.blockEntity.getBlockedByIds(user);
+    const blockedUserIds = await this.blockService.getBlockedUserIds(user);
+    const blockedByUserIds = await this.blockService.getBlockedByIds(user);
 
     const streams: IStream[] = await this.streamEntity.get({
       blockedUserIds,
