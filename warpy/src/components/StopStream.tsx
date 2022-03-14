d1ea34fb82220f8e10ba22b5f33cbeba76190182
Artem <griukov@protonmail.com>
2022-03-14 21:58:46 +0200

    fix leaving sreams

--- warpy/src/components/StopStream.tsx
+++ warpy/src/components/StopStream.tsx
@@ -1,18 +1,22 @@
-import {useStore} from '@app/store';
+import {useStoreShallow} from '@app/store';
 import {useNavigation} from '@react-navigation/native';
 import React, {useCallback} from 'react';
 import {IconButton} from './IconButton';
 
 export const StopStream = () => {
-  const api = useStore.use.api();
+  const [api, stream] = useStoreShallow(store => [store.api, store.stream]);
 
   const navigation = useNavigation();
 
   const onLeave = useCallback(async () => {
-    await api.stream.leave();
+    if (!stream) {
+      return;
+    }
+
+    await api.stream.leave(stream);
 
     navigation.navigate('Feed');
-  }, [navigation, api]);
+  }, [navigation, api, stream]);
 
   return (
     <IconButton onPress={onLeave} color="#ffffff" size={30} name="hand-wave" />
