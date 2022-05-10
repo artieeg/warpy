d32bf42d3d51b29e55a2490df35924f4473b874d
Artem <griukov@protonmail.com>
2022-05-10 15:18:13 +0300

    deleting invites

--- backend/src/stream/invite/invite.store.ts
+++ backend/src/stream/invite/invite.store.ts
@@ -1,15 +1,25 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { BotsEntity } from '@warpy-be/bots/bots.entity';
-import { UserEntity } from '@warpy-be/user/user.entity';
-import { IInvite, IStream, IUser } from '@warpy/lib';
+import { IInvite, IInviteBase, IStream, IUser } from '@warpy/lib';
 import cuid from 'cuid';
 import IORedis from 'ioredis';
 import { StreamEntity } from '../common/stream.entity';
 
+/** Store user data */
 const PREFIX_USER = 'user_';
+
+/** Store stream data */
 const PREFIX_STREAM = 'stream_';
+
+/** Store invite data */
 const PREFIX_INVITE = 'invite_';
+
+type InviteDTO = {
+  id: string;
+  stream?: string;
+  inviter_id: string;
+  invitee_id: string;
+};
 
 @Injectable()
 export class InviteStore implements OnModuleInit {
@@ -21,10 +31,23 @@ export class InviteStore implements OnModuleInit {
     this.redis = new IORedis(this.configService.get('participantStoreAddr'));
   }
 
-  static toDTO(data: any, inviter: IUser, invitee: IUser): IInvite {
+  private toBaseDTO(data: any): IInviteBase {
     return {
       id: data.id,
-      stream: data.stream ? StreamEntity.toStreamDTO(data.stream) : null,
+      stream_id: data.stream_id,
+      invitee_id: data.invitee_id,
+      inviter_id: data.inviter_id,
+    };
+  }
+
+  private toDTO(data: any, inviter: IUser, invitee: IUser): IInvite {
+    const stream = data.stream
+      ? StreamEntity.toStreamDTO(data.stream_id)
+      : undefined;
+
+    return {
+      ...this.toBaseDTO(data),
+      stream,
       invitee,
       inviter,
     };
@@ -47,26 +70,44 @@ export class InviteStore implements OnModuleInit {
 
     //Store stream data if provided
     if (stream) {
-      pipe.hmset(PREFIX_INVITE + stream.id, stream);
+      pipe.hmset(PREFIX_STREAM + stream.id, stream);
     }
 
     const invite_id = cuid();
 
-    //Store invite data
-    pipe.hmset(PREFIX_INVITE + invite_id, {
+    const invite: IInviteBase = {
       id: invite_id,
       inviter_id: inviter.id,
       invitee_id: invitee.id,
-      stream: stream.id,
-    });
+      stream_id: stream?.id,
+    };
+
+    //Store invite data
+    pipe.hmset(PREFIX_INVITE + invite_id, invite);
 
     await pipe.exec();
 
-    return {
-      id: invite_id,
-      stream: stream ?? null,
-      invitee,
-      inviter,
-    };
+    return this.toDTO(invite, inviter, invitee);
+  }
+
+  private async getInviteBase(invite_id: string) {
+    const data = await this.redis.hgetall(PREFIX_INVITE + invite_id);
+
+    return this.toBaseDTO(data);
+  }
+
+  async del(invite_id: string) {
+    const { inviter_id, invitee_id, stream_id } = await this.getInviteBase(
+      invite_id,
+    );
+
+    const pipe = this.redis.pipeline();
+
+    pipe.del(PREFIX_STREAM + stream_id);
+    pipe.del(PREFIX_INVITE + invite_id);
+    pipe.del(PREFIX_USER + invitee_id);
+    pipe.del(PREFIX_USER + inviter_id);
+
+    await pipe.exec();
   }
 }
--- lib/models/invite.ts
+++ lib/models/invite.ts
@@ -3,11 +3,17 @@ import { IUser } from "./user";
 
 type SentInviteState = "accepted" | "declined" | "unknown";
 
-export interface IInvite {
+export interface IInviteBase {
   id: string;
-  stream: IStream | null;
+  invitee_id: string;
+  inviter_id: string;
+  stream_id?: string;
+}
+
+export interface IInvite extends IInviteBase {
   invitee: IUser;
   inviter: IUser;
+  stream?: IStream;
 }
 
 export interface IInviteNotification extends IInvite {
