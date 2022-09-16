2ded8d2d95517e06b84758a18f2d473d6345acf0
Artem <griukov@protonmail.com>
2022-09-16 20:56:18 +0300

    ,

--- warpy-web-next/pages/invite/[id].tsx
+++ warpy-web-next/pages/invite/[id].tsx
@@ -1,16 +1,14 @@
 import React, { createRef } from "react";
-import { View, ScrollView, StyleSheet, Pressable } from "react-native";
+import { View, ScrollView, StyleSheet } from "react-native";
 import { Text, ButtonWithBackdrop } from "@warpy/components";
 import { NextPageContext } from "next";
 import { runNATSRequest } from "../../modules/comms";
-import { IAppInvite } from "@warpy/lib";
 import GooglePlay from "../../public/icons/google-play.svg";
 import AppStore from "../../public/icons/app-store.svg";
-import ChevronDown from "../../public/icons/chevron-down.svg";
-import Heart from "../../public/icons/heart.svg";
+import { AppInvite } from "@warpy/lib";
 
 type InviteProps = {
-  invite: IAppInvite;
+  invite: AppInvite;
 };
 
 export default function Invite({ invite }: InviteProps) {
@@ -57,63 +55,7 @@ export default function Invite({ invite }: InviteProps) {
               <AppStore height={35} width={35} />
             </ButtonWithBackdrop>
           </View>
-          <Pressable
-            onPress={() => {
-              ref.current?.scrollToEnd();
-            }}
-            style={[
-              styles.autoscroll,
-              {
-                paddingBottom: 60,
-              },
-            ]}
-          >
-            <Text size="small" color="info">
-              message from the dev
-            </Text>
-            <ChevronDown fill="#7B7B7B" width={30} height={30} />
-          </Pressable>
         </View>
-
-        <div className="notch-padding" style={{ flex: 1 }}>
-          <Pressable
-            onPress={() => {
-              ref.current?.scrollTo({ y: 0 });
-            }}
-            style={[styles.autoscroll, { paddingTop: 60, paddingBottom: 60 }]}
-          >
-            <View style={{ transform: [{ rotate: "180deg" }] }}>
-              <ChevronDown fill="#7B7B7B" width={30} height={30} />
-            </View>
-            <Text size="small" color="info">
-              back to the invite
-            </Text>
-          </Pressable>
-          <Text style={{ marginBottom: 20 }} size="large">
-            hi
-            <br />
-            <br />
-          </Text>
-
-          <Text size="small">
-            i’ve put a ton of love in this app over the past few months, adding
-            a ton of cool features and having even more in the pipeline <br />
-            <br />
-            keeping things free with no ads as well <br />
-            <br />
-            come join to get the vibe going!
-          </Text>
-
-          <View
-            style={{
-              marginTop: 40,
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            <Heart width={40} height={40} fill="#BDF971" />
-          </View>
-        </div>
       </ScrollView>
     </View>
   );
